d116522f83c1299dbbdbcf63180d2fe2df2dfd76
Hazz-i <wahidh697@gmail.com>
2025-01-24 08:30:42 +0700

    add: dinamis content

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
+import axios from "axios";
 import BookSection from "./components/bookSection";
 import Header from "./components/header";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { useEffect, useState } from "react";
-
-const buku = [
-	{ id: 1, title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
-	{ id: 2, title: "1984", author: "George Orwell", year: 1949 },
-	{ id: 3, title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-	{ id: 4, title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
-	{ id: 5, title: "The Catcher in the Rye", author: "J.D. Salinger", year: 1951 },
-	{ id: 6, title: "Moby-Dick", author: "Herman Melville", year: 1851 },
-	{ id: 7, title: "War and Peace", author: "Leo Tolstoy", year: 1869 },
-	{ id: 8, title: "Crime and Punishment", author: "Fyodor Dostoevsky", year: 1866 },
-	{ id: 9, title: "The Brothers Karamazov", author: "Fyodor Dostoevsky", year: 1880 },
-	{ id: 10, title: "Brave New World", author: "Aldous Huxley", year: 1932 },
-	{ id: 11, title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 },
-	{ id: 12, title: "Anna Karenina", author: "Leo Tolstoy", year: 1877 },
-	{ id: 13, title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1954 },
-	{ id: 14, title: "Jane Eyre", author: "Charlotte Brontë", year: 1847 },
-	{ id: 15, title: "Wuthering Heights", author: "Emily Brontë", year: 1847 },
-	{ id: 16, title: "The Odyssey", author: "Homer", year: -800 },
-	{ id: 17, title: "The Iliad", author: "Homer", year: -750 },
-	{ id: 18, title: "Les Misérables", author: "Victor Hugo", year: 1862 },
-	{ id: 19, title: "Don Quixote", author: "Miguel de Cervantes", year: 1605 },
-	{ id: 20, title: "Frankenstein", author: "Mary Shelley", year: 1818 },
-];
 
 export default function App() {
 	const [loading, setLoading] = useState(false);
@@ -34,17 +12,26 @@ export default function App() {
 	const [keyword, setKeyword] = useState("");
 
 	const handleSearch = () => {
-		const filteredBooks = buku.filter((book) =>
+		const filteredBooks = books.filter((book) =>
 			book.title.toLowerCase().includes(keyword.toLowerCase())
 		);
 		setBooks(filteredBooks);
 	};
-	useEffect(() => {
+
+	const fetchBooks = async () => {
 		setLoading(true);
-		setTimeout(() => {
-			setBooks(buku);
+		try {
+			const response = await axios.get(`${import.meta.env.VITE_API_BE}/books`);
+			setBooks(response.data);
+		} catch (error) {
+			console.error("Error fetching books:", error.message, error.response);
+		} finally {
 			setLoading(false);
-		}, 2000);
+		}
+	};
+
+	useEffect(() => {
+		fetchBooks();
 	}, []);
 
 	return (
